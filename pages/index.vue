3fdd68271470c1adf8543f0c2402b5bf6d3a4083
thibault <thibault.rey@akretion.com>
2022-12-15 23:51:11 +0100

    [IMP] Add user icon on header

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
   HOMEPAGE
+
 </template>
 <script lang="ts">
 export default {
   name: 'index',
-  setup () {
-    
+  data() {
+    return {
+      test: null
+    }
+  },
+  computed: {
+    user() {
+      return useCurrentUser() || null
+    }
+  },
+  methods: {
+    async getUser() {
+      const customerService = useShopinvaderServices()?.customer || {}
+      console.log(await customerService.get())
+    },
+    async signin () {
+      await useAuth()?.login()
+
+    },
+    async logout () {
+      await useAuth()?.logout()
+
+    }
   }
 }
 </script>
