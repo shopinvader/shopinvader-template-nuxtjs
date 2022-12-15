372c51ef9142c8e664f03143022ca27bd0917d6b
thibault <thibault.rey@akretion.com>
2022-12-15 23:54:13 +0100

    [FIX] remove debugs

--- modules/shopinvader-services/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Shopinvader } from './Shopinvader'
-export { ShopinvaderProviders } from './ShopinvaderProviders'
-export { ShopinvaderService } from './ShopinvaderService'
-export { ShopinvaderServiceList } from './ShopinvaderServiceList'
-export { ShopinvaderOptions, ShopinvaderElasticIndexOptions, ShopinvaderElasticOptions, ShopinvaderErpOptions } from './config'
-export { ElasticFetch, ErpFetch } from "@shopinvader/fetch"
