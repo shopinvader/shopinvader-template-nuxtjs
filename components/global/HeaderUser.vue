509c26f1cbbe39068fd9d907c484560c5621dea6
thibault <thibault.rey@akretion.com>
2023-01-19 13:10:58 +0100

    [FIX] User dropdown in header

--- components/global/HeaderUser.vue
+++ components/global/HeaderUser.vue
@@ -6,7 +6,7 @@
       </button>
     </template>
     <template v-else>
-      <div class="dropdown dropdown-end">
+      <div class="dropdown-end dropdown">
         <label tabindex="0" class="btn-ghost btn text-info">
           <Icon icon="ph:user" class="text-3xl text-blue-500 lg:text-5xl" />
         </label>
@@ -15,14 +15,16 @@
           class="dropdown-content menu rounded-box w-64 bg-base-100 shadow"
         >
           <li>
+            <nuxt-link to="/account/addresses">
+              <icon icon="ph:address-book" class="pr-2 text-2xl"></icon>
+              {{ $t('account.address.title') }}
+            </nuxt-link>
+          </li>
+          <li class="border-t">
             <button type="button" @click="logout">
+              <icon icon="ph:power" class="pr-2 text-2xl"></icon>
               {{ $t('account.logout') }}
             </button>
-          </li>
-          <li>
-            <nuxt-link to="/account/addresses">
-              {{ $t('account.address') }}
-            </nuxt-link>
           </li>
         </ul>
       </div>
@@ -30,8 +32,12 @@
   </div>
 </template>
 <script lang="ts">
+import { Icon } from '@iconify/vue'
 export default defineNuxtComponent({
   name: 'HeaderUser',
+  components: {
+    icon: Icon
+  },
   computed: {
     user() {
       return useCurrentUser() || null
