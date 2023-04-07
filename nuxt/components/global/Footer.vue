0fe486426a28282c051436cdbb1b14391aff4d79
thibault <thibault.rey@akretion.com>
2023-04-07 16:04:31 +0200

    [IMP] CMS content on footer

--- nuxt/components/global/Footer.vue
+++ nuxt/components/global/Footer.vue
@@ -1,22 +1,42 @@
 <template>
-  <footer class="footer border-t p-10 text-base-content">
-    <div>
-      <logo></logo>
-      FOOTER
-    </div>
-  </footer>
+  <div class="bg-gray-800 text-white">
+    <footer class="footer">
+      <div>
+        <cms-image v-if="footer?.logo" :image="footer.logo"></cms-image>
+        <div v-if="footer?.baseline" class="max-md:hidden">
+          {{ footer.baseline }}
+        </div>
+      </div>
+      <div v-for="col in footer.content" :key="col.id">
+        <span class="footer-title">{{ col.title }}</span>
+        <p v-html="markdown(col.text)" />
+      </div>
+    </footer>
+  </div>
 </template>
 <script lang="ts">
-import Logo from '~/components/global/Logo.vue'
-export default {
+import { Footer } from '~~/models/cms/Footer'
+import ImageVue from '~~/components/cms/shared/Image.vue'
+export default defineNuxtComponent({
   name: 'global-footer',
   components: {
-    logo: Logo
+    'cms-image': ImageVue
+  },
+  data() {
+    return {
+      footer: {} as Footer
+    }
+  },
+  async asyncData() {
+    const { getFooter } = useCMS()
+    return {
+      footer: await getFooter()
+    }
   }
-}
+})
 </script>
 <style lang="scss">
 .footer {
-  @apply container mx-auto border-t px-4 sm:px-6 md:px-8;
+  @apply container mx-auto bg-gray-800 p-4 py-10;
 }
 </style>
