1881c8be279060ee4c69a8fc97db226b1d6a25aa
thibault <thibault.rey@akretion.com>
2023-06-16 16:15:58 +0200

    [IMP] add header meta data on category and product page

--- pages/template/CategoryPage.vue
+++ pages/template/CategoryPage.vue
@@ -4,12 +4,29 @@
   </div>
 </template>
 <script lang="ts" setup>
+import { Category } from '~/models/Category'
+
 /**
  * Get Category from payload set on the route middleware
  * in the shopinvader plugin
  * */
-const category = useNuxtApp().payload?.data?.entity || null
+const category: Category | null = useNuxtApp().payload?.data?.entity || null
 if (!category) {
   throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
 }
+useHead({
+  title: category.name,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: category.metaDescription || ''
+    },
+    {
+      hid: 'keywords',
+      name: 'keywords',
+      content: category.metaKeywords || ''
+    }
+  ]
+})
 </script>
--- pages/template/ProductPage.vue
+++ pages/template/ProductPage.vue
@@ -4,12 +4,29 @@
   </div>
 </template>
 <script lang="ts" setup>
+import { Product } from '~/models'
+
 /**
  * Get Category from payload set on the route middleware
  * in the shopinvader plugin
  * */
-const product = useNuxtApp().payload?.data?.entity || null
+const product: Product | null = useNuxtApp().payload?.data?.entity || null
 if (!product) {
   throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
 }
+useHead({
+  title: product.name,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: product.metaDescription || ''
+    },
+    {
+      hid: 'keywords',
+      name: 'keywords',
+      content: product.metaKeywords || ''
+    }
+  ]
+})
 </script>
