22f169205e8c259e424692ea7e21c325a77203f7
thibault <thibault.rey@akretion.com>
2023-07-07 17:45:18 +0200

    [IMP] local switcher style

--- components/global/LocalSwitcher.vue
+++ components/global/LocalSwitcher.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-if="availableLocales.length > 1" class="localeposition dropdown">
-    <label tabindex="0" class="m-1">
+  <div v-if="availableLocales.length > 1" class="locale-switcher dropdown">
+    <label tabindex="0" class="locale-switcher__btn">
       <span class="font-semibold uppercase">
         <icon :icon="currentLocalIcon" class="text-4xl" />
       </span>
-      <icon icon="ph:caret-down-light" />
+      <icon icon="ph:caret-down-light" class="text-lg" />
     </label>
-    <ul class="dropdown-content menu rounded-box menu-compact bg-base-100 p-2">
+    <ul
+      class="dropdown-content menu rounded-box menu-compact bg-base-100 p-2 shadow"
+    >
       <li v-for="item in availableLocales" :key="item.code">
         <a :key="item.code" :href="localePath('index', item.code)">
           <icon :icon="item.icon" class="text-2xl" />{{ item.name }}
@@ -29,22 +31,9 @@ const availableLocales = computed(() => {
 </script>
 
 <style lang="scss">
-.localeposition {
-  display: block ruby;
-}
-.localeswitcher {
-  display: flex;
-
-  .localeswitcher-item {
-    @apply border-r px-2;
-
-    &:last-child {
-      @apply border-r-0;
-    }
-
-    &--active {
-      @apply font-bold;
-    }
+.locale-switcher {
+  &__btn {
+    @apply m-1 flex items-center;
   }
 }
 </style>
