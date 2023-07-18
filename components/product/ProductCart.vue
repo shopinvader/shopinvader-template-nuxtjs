392eb102f0d0be76475f347769a95d7b6a84c93f
Dora Jurcevic <dora.jurcevic@akretion.com>
2023-07-18 13:06:24 +0200

    fix: drawer placed on productCart component

--- components/product/ProductCart.vue
+++ components/product/ProductCart.vue
@@ -10,25 +10,52 @@
         <Icon icon="clarity:shopping-bag-line" class="text-xl lg:text-2xl" />
         <Icon icon="ic:outline-plus" class="add-icon__plus" />
       </div>
-      <span class="add-label">{{ $t('product.cart.add') }}</span>
+      <span class="add-label">{{ $t('product.cart.add') }} </span>
     </button>
+
     <cart-line-qty v-else :line="line"></cart-line-qty>
   </div>
+  <aside-drawer :open="cartDrowerOpened" @close="closeDrawer">
+    <template #header>
+      <div class="w-full text-2xl">
+        {{ $t('cart.title') }}
+      </div>
+    </template>
+    <template #content>
+      <transition name="cart-confirm">
+        <div v-if="cartModified" class="cart-confirmation">
+          <icon icon="mdi:check" class="mr-2" />
+          {{ $t('cart.confirmation') }}
+        </div>
+      </transition>
+      <cart @update-cart="test"> </cart>
+    </template>
+  </aside-drawer>
 </template>
 <script lang="ts">
 import { PropType } from 'vue'
 import { CartLine } from '~~/models'
 import { Product } from '~~/models/Product'
 import CartLineQtyVue from '../cart/CartLineQty.vue'
+import AsideDrawer from '~/components/global/AsideDrawer.vue'
+import Cart from '~/components/cart/Cart.vue'
 export default {
   name: 'ProductCart',
   components: {
-    'cart-line-qty': CartLineQtyVue
+    'cart-line-qty': CartLineQtyVue,
+    'aside-drawer': AsideDrawer,
+    cart: Cart
   },
   props: {
     product: {
       type: Object as PropType<Product>,
       required: true
+    }
+  },
+  data() {
+    return {
+      cartDrowerOpened: false as boolean,
+      cartModified: false as boolean
     }
   },
   computed: {
@@ -40,6 +67,19 @@ export default {
           (line: CartLine) => line.productId === this.product.id
         ) || null
       )
+    },
+    lines(): CartLine[] {
+      const cartService = useShopinvaderService('cart')
+      const cart = cartService.getCart()
+      return cart?.value?.lines as CartLine[]
+    }
+  },
+  watch: {
+    lines(): void {
+      this.cartModified = true
+      setTimeout(() => {
+        this.cartModified = false
+      }, 1000)
     }
   },
   methods: {
@@ -47,7 +87,14 @@ export default {
       const cartService = useShopinvaderService('cart')
       if (cartService && this.product?.id !== null) {
         cartService.addItem(this.product.id, 1)
+        this.cartDrowerOpened = true
       }
+    },
+    closeDrawer(): void {
+      this.cartDrowerOpened = false
+    },
+    test() {
+      console.log('updatedcart')
     }
   }
 }
@@ -68,6 +115,53 @@ export default {
         background-color: inherit;
       }
     }
+  }
+}
+
+.aside-drawer {
+  @apply fixed left-auto right-0 top-0 z-50 flex items-start justify-start;
+  &-enter-to,
+  &-leave-from {
+    opacity: 1;
+
+    .aside-drawer__side {
+      right: 0;
+    }
+  }
+
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+
+    .aside-drawer__side {
+      right: -100%;
+    }
+  }
+  .side__header {
+    @apply w-full 
+  }
+  .side__content {
+    @apply h-full;
+    .cart-confirmation {
+      @apply flex w-full  items-center bg-gray-200 p-2 align-middle text-success;
+    }
+    .cart-confirm-enter-active,
+    .cart-confirm-leave-active {
+      @apply transition-all duration-200 ease-in-out;
+    }
+    .cart-confirm-enter-from,
+    .cart-confirm-leave-to {
+      @apply bg-gray-100;
+    }
+  }
+  &__side {
+    @apply absolute left-auto right-0 h-screen w-10/12 max-w-screen-sm overflow-y-auto bg-white shadow-xl ;
+  }
+  .side__footer {
+    @apply hidden;
+  }
+  .cart {
+    @apply flex flex-col place-content-between justify-between w-full;
   }
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,16 +7,6 @@
     <main>
       <slot name="body"> </slot>
       <slot></slot>
-      <aside-drawer :open="cartDrowerOpened" @close="closeDrawer">
-        <template #header>
-          <div class="text-2xl">
-            {{ $t('cart.title') }}
-          </div>
-        </template>
-        <template #content>
-          <cart> </cart>
-        </template>
-      </aside-drawer>
     </main>
     <footer-main></footer-main>
     <navbar-bottom class="md:hidden"></navbar-bottom>
@@ -28,9 +18,6 @@ import HeaderMain from '~/components/global/header/HeaderMain.vue'
 import FooterMain from '~/components/global/FooterMain.vue'
 import Notifications from '~/components/global/Notifications.vue'
 import NavbarBottomVue from '~/components/global/NavbarBottom.vue'
-import AsideDrawer from '~/components/global/AsideDrawer.vue'
-import Cart from '~/components/cart/Cart.vue'
-import { CartLine } from '~~/models'
 export default defineNuxtComponent({
   name: 'Default',
   components: {
@@ -38,32 +25,6 @@ export default defineNuxtComponent({
     'footer-main': FooterMain,
     'notifications-vue': Notifications,
     'navbar-bottom': NavbarBottomVue,
-    'aside-drawer': AsideDrawer,
-    'cart': Cart
-  },
-  data() {
-    return {
-      cartDrowerOpened: false as boolean
-    }
-  },
-  computed: {
-    lines(): CartLine[] {
-      const cartService = useShopinvaderService('cart')
-      const cart = cartService.getCart()
-      return cart?.value?.lines as CartLine[]
-    }
-  },
-  watch: {
-    lines(lines, oldLines): void {
-      if (lines.length > oldLines.length) {
-        this.cartDrowerOpened = true
-      }
-    }
-  },
-  methods: {
-    closeDrawer(): void {
-      this.cartDrowerOpened = false
-    }
   }
 })
 </script>
@@ -75,20 +36,5 @@ main {
 #app {
   @apply flex min-h-screen flex-col;
 
-  .aside-drawer {
-   .side__content {
-    @apply h-full;
-   }
-    &__side {
-      @apply absolute right-0 left-auto h-screen w-10/12 max-w-screen-sm overflow-y-auto bg-white shadow-xl;
-    }
-    .side__footer {
-      @apply hidden;
-    }
-    .cart {
-      @apply flex flex-col place-content-between justify-between;
-    }
-
-  }
 }
 </style>
