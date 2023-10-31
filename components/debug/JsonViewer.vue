22fa66a4538ff3c11021fc39a3a18fcd4587a88d
thibault <thibault.rey@akretion.com>
2023-10-31 17:33:12 +0100

    [IMP] Hide JSON Viewer in production mode

--- components/debug/JsonViewer.vue
+++ components/debug/JsonViewer.vue
@@ -1,32 +1,34 @@
 <template>
   <ClientOnly>
-    <div class="fixed bottom-3 right-3">
-      <button
-        type="button"
-        class="btn-warning btn-circle btn text-white shadow-xl"
-        @click="toggle = !toggle"
-      >
-        JSON
-      </button>
-    </div>
-    <input
-      id="my-modal-4"
-      type="checkbox"
-      class="modal-toggle"
-      :checked="toggle"
-    />
-    <div class="modal cursor-pointer" @click="toggle = !toggle">
-      <div class="modal-box relative w-11/12 max-w-5xl p-0">
-        <div v-if="data" class="mockup-code">
-          <pre
-            v-if="data.constructor"
-            data-prefix="Type"
-            class="bg-warning text-warning-content"
-          ><code>{{ data.constructor.name }}</code></pre>
-          <pre><code>{{ data }}</code></pre>
+    <template v-if="displayed">
+      <div class="fixed bottom-3 right-3">
+        <button
+          type="button"
+          class="btn-warning btn-circle btn text-white shadow-xl"
+          @click="toggle = !toggle"
+        >
+          JSON
+        </button>
+      </div>
+      <input
+        id="my-modal-4"
+        type="checkbox"
+        class="modal-toggle"
+        :checked="toggle"
+      />
+      <div class="modal cursor-pointer" @click="toggle = !toggle">
+        <div class="modal-box relative w-11/12 max-w-5xl p-0">
+          <div v-if="data" class="mockup-code">
+            <pre
+              v-if="data.constructor"
+              data-prefix="Type"
+              class="bg-warning text-warning-content"
+            ><code>{{ data.constructor.name }}</code></pre>
+            <pre><code>{{ data }}</code></pre>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </ClientOnly>
 </template>
 <script lang="ts">
@@ -38,8 +40,13 @@ export default {
     }
   },
   setup() {
+    const displayed = ref(false)
+    const app = useNuxtApp()
     const toggle = ref(false)
-    return { toggle }
+    if(process?.env?.NODE_ENV === 'development') {
+      displayed.value = true
+    }
+    return { toggle, displayed }
   }
 }
 </script>
