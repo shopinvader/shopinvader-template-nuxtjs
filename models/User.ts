c44eb4f6667176e8a2ad75a6c636f63160e28cc0
thibault <thibault.rey@akretion.com>
2023-11-03 14:02:28 +0100

    [IMP] Add name attribute on user model

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,9 @@
 export class User {
+  name: string | null = null
   login: string | null = null
   role: string | null = null
   constructor(data: any) {
+    this.name = data?.name || null
     this.login = data?.login || null
     this.role = data?.role || 'default'
   }
