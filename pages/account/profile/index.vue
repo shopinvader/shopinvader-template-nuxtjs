4dcc6a688cee2baf187689c97ee8ddfdb1de620b
thibault <thibault.rey@akretion.com>
2024-01-11 21:31:06 +0100

    [IMP] Add Profile component

--- pages/account/profile/index.vue
+++ pages/account/profile/index.vue
@@ -2,48 +2,7 @@
 
   <account-layout slug="account-profile">
     <template #content>
-      <div v-if="user" class="account-profile">
-        <div class="profile">
-          <div class="profile-email">
-            <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text">
-                  {{ $t('account.address.email') }}
-                </span>
-              </label>
-              <input
-                type="text"
-                :value="user?.login || ''"
-                class="input input-bordered w-full max-w-xs"
-                disabled
-              />
-            </div>
-          </div>
-          <div class="profile-email">
-            <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text">
-                    {{ $t('account.login.password') }}
-                </span>
-              </label>
-              <input
-                type="text"
-                value="**********"
-                class="input input-bordered w-full max-w-xs"
-                disabled
-              />
-              <label class="label">
-                <span class="label-text-alt"></span>
-                <span class="label-text-alt">
-                  <nuxt-link :to="{ path: '/account/password-reset', query: { email: user?.login } }" class="btn btn-link btn-xs">
-                    {{ $t('account.login.forgot_password') }}
-                  </nuxt-link>
-                </span>
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
+      <account-profile  v-if="user" :profile="user"/>
     </template>
   </account-layout>
 </template>
@@ -67,8 +26,3 @@ export default defineNuxtComponent({
   }
 })
 </script>
-<style lang="scss">
-  .account-profile {
-    @apply border-b pb-4;
-  }
-</style>
