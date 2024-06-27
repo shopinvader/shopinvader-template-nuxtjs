6ee54cb7501457e3c2cc58532baa511fb3662c00
Thibault Rey <thibault.rey@akretion.com>
2024-06-27 11:16:23 +0200

    refacture(models)

--- components/search/SearchRangePrice.vue
+++ components/search/SearchRangePrice.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="loaded || 1 == 1" class="searchfilter filter-price-range">
+  <div v-if="loaded" class="searchfilter filter-price-range">
     <slot name="title" :title="title">
       <div class="searchfilter__header" @click="opened = !opened">
         <div class="header__title">
