186fc69b5a3a04145192e3ba99f464ec34886210
egervy <gervy.emmanuel@gmail.com>
2024-07-02 15:46:52 +0200

    [FIX] User must extand Model

--- models/User.ts
+++ models/User.ts
@@ -1,8 +1,11 @@
-export class User {
+import { Model } from './Model'
+
+export class User extends Model {
   name: string | null = null
   login: string | null = null
   role: string | null = null
   constructor(data: any) {
+    super(data)
     this.name = data?.name || null
     this.login = data?.login || data?.email || null
     this.role = data?.role || data?.pricelist_id || 'default'
