bb3c1a71e18881ad2f8ead19519b1e453bfffa13
Dora Jurcevic <dora.jurcevic@akretion.com>
2024-07-03 15:20:14 +0200

    add: xlarge img added to model and to zoom

--- models/ProductImageSet.ts
+++ models/ProductImageSet.ts
@@ -4,11 +4,13 @@ export class ProductImageSet extends Model {
   small: ProductImage | null
   medium: ProductImage | null
   large: ProductImage | null
+  xlarge: ProductImage | null
 
   constructor(data: any) {
     super(data)
     this.small = data && data.small ? new ProductImage(data.small) : null
     this.medium = data && data.medium ? new ProductImage(data.medium) : null
     this.large = data && data.large ? new ProductImage(data.large) : null
+    this.xlarge = data && data.xlarge? new ProductImage(data.xlarge) : null
   }
 }
