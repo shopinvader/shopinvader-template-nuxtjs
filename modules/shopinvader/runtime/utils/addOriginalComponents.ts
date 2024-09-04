7cbccc8242e9c49f406cfee41570ebad37e5c584
Thibault Rey <thibault.rey@akretion.com>
2024-09-04 11:31:23 +0200

    Revert "fix(shopinvader module) : load original component only if it's necessary"
    This reverts commit 793b42e7aff54f61d30a2873c942b5d3511ef74a.

--- modules/shopinvader/runtime/utils/addOriginalComponents.ts
+++ modules/shopinvader/runtime/utils/addOriginalComponents.ts
@@ -1,5 +1,5 @@
-import { addComponent, createResolver, resolveFiles } from '@nuxt/kit'
-import { type Nuxt } from 'nuxt/schema'
+import { createResolver, resolveFiles, addComponent } from '@nuxt/kit'
+import { Nuxt } from 'nuxt/schema'
 
 /**
  * addOriginalComponents add components from other original layers
@@ -7,30 +7,23 @@ import { type Nuxt } from 'nuxt/schema'
  * @param nuxt
  */
 export const addOriginalComponents = async (nuxt: Nuxt) => {
-
   const layers = nuxt.options._layers
-  let loadedFiles:string[] = []
   if(layers.length > 1) {
     for(let layer of layers) {
-      const {resolve} = createResolver(layer.cwd)
-      const files = await resolveFiles(resolve('./components'), '**/*.vue')
-      if(layer.cwd == nuxt.options.srcDir) {
-        loadedFiles = files.map(f => f.replace(layer.cwd, ''))
-      } else {
-
+      if(layer.cwd !== nuxt.options.srcDir) {
+        const {resolve} = createResolver(layer.cwd)
+        const files = await resolveFiles(resolve('./components'), '**/*.vue')
         for(let filePath of files) {
           /* get name from filePath  */
-          const path = filePath.replace(layer.cwd, '')
-          if(loadedFiles.includes(path)) {
-            const name = `Original${filePath.replace(/\.vue$/, '').split('/').pop()}`
-            await addComponent({
-              name,
-              filePath,
-              pascalName: name
-            })
-          }
+          const name = `Original${filePath.replace(/\.vue$/, '').split('/').pop()}`
+          await addComponent({
+            name,
+            filePath,
+            pascalName: name
+          })
         }
       }
     }
+    console.info('Original components added')
   }
 }
