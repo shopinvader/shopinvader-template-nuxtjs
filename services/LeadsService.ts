c3e2832abfc24404b766e4af6572df1c780b87e2
Thibault Rey <thibault.rey@akretion.com>
2024-09-09 16:24:32 +0200

    fix(lead): fix lead services

--- services/LeadsService.ts
+++ services/LeadsService.ts
@@ -1,12 +1,20 @@
-import { ErpFetch } from '@shopinvader/fetch'
+import type { $Fetch } from 'ofetch'
 import { Lead, type LeadsResult } from '#models'
-import { Service } from '#services'
+import { BaseServiceLocalized } from './BaseServiceLocalized'
 
-export class LeadsService extends Service {
-  provider: ErpFetch | null = null
-  constructor(provider: ErpFetch) {
-    super()
-    this.provider = provider
+
+export class LeadsService extends BaseServiceLocalized {
+  public ofetch: $Fetch
+  // Base url of the erp API (eg; https://myerp.mycompany.com/shopinvader)
+  public erpBaseUrl: string
+  // Note: you can change the endpoint to match your API endpoint before the Init
+  public endpoint: string = ''
+  // The urlEndpoint is the erpBaseUrl + the endpoint
+  public urlEndpoint: string = ''
+  constructor(isoLocale: string, ofetch: $Fetch, erpBaseUrl: string) {
+    super(isoLocale)
+    this.ofetch = ofetch
+    this.erpBaseUrl = erpBaseUrl
   }
 
   async create(lead: Lead, files: FileList | null): Promise<LeadsResult> {
@@ -16,7 +24,10 @@ export class LeadsService extends Service {
     if (files) {
       formData.append(`files`, files[0])
     }
-    const result = await this.provider?.post("leads", formData)
+    const result = await this.ofetch("leads", {
+      method: "POST",
+      body: formData
+    })
     return {
       size: result?.size || 0,
       data: result?.data?.map((item: any) => new Lead(item))
