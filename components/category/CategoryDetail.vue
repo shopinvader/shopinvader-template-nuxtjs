0df0119fbcb34ce6eb4b153d0d1f14014dd97b40
Thibault Rey <thibault.rey@akretion.com>
2024-09-09 15:09:12 +0200

    refactor(category)

--- components/category/CategoryDetail.vue
+++ components/category/CategoryDetail.vue
@@ -28,73 +28,60 @@
     </template>
   </search-product>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import type { CategoryParent } from '#models'
 import { Category } from '#models'
 import esb from 'elastic-builder'
 import type { PropType } from 'vue'
-import JsonViewer from '~/components/debug/JsonViewer.vue'
-import SearchProduct from '~/components/search/SearchProduct.vue'
+
 import { useHistoryStore } from '~/stores/history'
-export default {
-  name: 'CategoryPage',
-  components: {
-    'search-product': SearchProduct,
-    'json-viewer': JsonViewer
-  },
-  layout: 'default',
-  props: {
-    category: {
-      type: Object as PropType<Category>,
-      required: true
-    }
-  },
-  setup(props) {
-    const localePath = useLocalePath()
-    useHistoryStore()?.setLastCategory(props.category)
-    return {
-      localePath
-    }
-  },
-  computed: {
-    breadcrumb() {
-      const localePath = useLocalePath()
-      const t = useI18n().t
-      let current: Category | CategoryParent | null = this.category
-      const items = [current]
-      while (current?.parent !== null) {
-        current = current?.parent || null
-        if (current?.name) {
-          items.push(current)
-        }
-      }
-      items.push(new Category({ name: t('navbar.home'), url_key: localePath({ path: '/' }) }))
-      items.reverse()
-      return items
-    }
-  },
-  methods: {
-    providerFunction(body: any) {
-      const productService = useShopinvaderService('products')
-      return productService?.search(body)
-    },
-    query() {
-      let query = esb.matchAllQuery()
+const props = defineProps({
+  category: {
+    type: Object as PropType<Category>,
+    required: true
+  }
+})
+const localePath = useLocalePath()
 
-      if (this?.category?.id !== null) {
-        const id = this.category.id + '' // convert to string
-        query = esb
-          .nestedQuery()
-          .path('categories')
-          .query(
-            esb.boolQuery()
-            .must([esb.matchQuery('categories.id', id)])
-            .should(esb.termQuery('main', true))
-          )
-
-      }
-      return query
+const breadcrumb = computed(() => {
+  const t = useI18n().t
+  let current: Category | CategoryParent | null = props.category
+  const items = [current]
+  while (current?.parent !== null) {
+    current = current?.parent || null
+    if (current?.name) {
+      items.push(current)
     }
   }
+  items.push(new Category({ name: t('navbar.home'), url_key: localePath({ path: '/' }) }))
+  items.reverse()
+  return items
+})
+
+const providerFunction = (body: any) => {
+  const productService = useShopinvaderService('products')
+  return productService?.search(body)
 }
+
+const query = () => {
+  let query = esb.matchAllQuery()
+
+  if (props?.category?.id !== null) {
+    const id = props.category.id + '' // convert to string
+    query = esb
+      .nestedQuery()
+      .path('categories')
+      .query(
+        esb
+          .boolQuery()
+          .must([esb.matchQuery('categories.id', id)])
+          .should(esb.termQuery('main', true))
+      )
+  }
+  return query
+}
+
+onMounted(() => {
+  useHistoryStore()?.setLastCategory(props.category)
+})
 </script>
