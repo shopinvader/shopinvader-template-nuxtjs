ef1fc1181dbd7d8501c82e57ae488d199eab9174
Thibault Rey <thibault.rey@akretion.com>
2024-09-09 14:25:30 +0200

    Update nuxtjs robots + sitemap packages

--- modules/shopinvader/runtime/plugins/services.ts
+++ modules/shopinvader/runtime/plugins/services.ts
@@ -1,13 +1,5 @@
 import { useRuntimeConfig } from '#app'
-import type {
-  ShopinvaderServiceList as ServiceList,
-  ShopinvaderConfig,
-  ShopinvaderProvidersList
-} from '../types/ShopinvaderConfig'
-import { initProviders } from './providers/index'
-
 import type { AuthAPIConfig, AuthOIDCConfig, AuthService } from '#services'
-
 import {
   AddressService,
   AuthCredentialService,
@@ -23,27 +15,49 @@ import {
   SettingService,
   LeadsService
 } from '#services'
+import type {
+  ShopinvaderServiceList as ServiceList,
+  ShopinvaderConfig,
+  ShopinvaderProvidersList
+} from '../types/ShopinvaderConfig'
 import type { ErpFetchObservable } from './providers/ErpFetchObservable'
+import { initProviders } from './providers/index'
+
+declare global {
+  interface Shopinvader {
+    services: ShopinvaderServiceList
+    providers: ShopinvaderProvidersList
+  }
+}
 
 declare global {
   interface ShopinvaderServiceList extends ServiceList {}
 }
 
-const isValidURL = (url: string) => {
-  try {
-    new URL(url)
-    return true
-  } catch (e) {
-    return false
+declare module '#app' {
+  interface NuxtApp {
+    $shopinvader: Shopinvader
   }
 }
 
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $shopinvader: Shopinvader
+  }
+}
+
+/*
+ * This plugin is used to initialize all the services used in the app.
+ * It also provides the providers to fetch data from the ERP and ElasticSearch.
+ */
 export default defineNuxtPlugin(async (nuxtApp) => {
   const app = useNuxtApp()
+  // Get the shopinvader config from the runtime config
   const shopinvaderConfig = useRuntimeConfig()?.public?.shopinvader as ShopinvaderConfig
   let config = {
     ...shopinvaderConfig
   }
+  // If the ERP URL is not valid, we use the current URL
   const url = config?.erp?.url || ''
   if (!url || !isValidURL(url)) {
     const url = useRequestURL()
@@ -56,39 +70,40 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     }
   }
 
+  // Get locale from i18n
   const i18nOptions: any = app.$i18n || {}
   const isoLocale: string = i18nOptions?.localeProperties?.value?.iso || 'fr_fr'
-  //  ERP and Elastic providers (fetches)
+  // Init ERP and Elastic providers (fetches)
   const providers = initProviders(config as ShopinvaderConfig, isoLocale)
-  const erp = providers.erp
-  const elasticProducts = providers.products
-  const elasticCategories = providers.categories
+  const erpProvider = providers.erp
+  const productsProvider = providers.products
+  const categoriesProvider = providers.categories
   const elasticSearch = providers.elasticsearch
 
+  // Create the auth service, depending on the config
   let auth: AuthService | null = null
   if (config?.auth?.type) {
-    /** Auth Service */
-    const profile = config.auth?.profile
+    const authConfig = config.auth?.profile
     if (config.auth.type === 'oidc') {
-      auth = new AuthOIDCService(erp as ErpFetchObservable, profile as AuthOIDCConfig)
+      auth = new AuthOIDCService(erpProvider as ErpFetchObservable, authConfig as AuthOIDCConfig)
     } else {
-      auth = new AuthCredentialService(erp, profile as AuthAPIConfig)
+      auth = new AuthCredentialService(erpProvider, authConfig as AuthAPIConfig)
     }
   }
-
+  // Create all other services
   const services: ShopinvaderServiceList = {
     auth,
-    products: new ProductService(elasticProducts),
-    categories: new CategoryService(elasticCategories),
+    products: new ProductService(productsProvider),
+    categories: new CategoryService(categoriesProvider),
     catalog: new CatalogService(elasticSearch),
-    cart: new CartService(erp),
-    settings: new SettingService(erp),
-    addresses: new AddressService(erp),
-    sales: new SaleService(erp),
-    deliveryCarriers: new DeliveryCarrierService(erp),
-    payment: new PaymentService(erp),
-    customer: new CustomerService(erp),
-    leads: new LeadsService(erp)
+    cart: new CartService(erpProvider),
+    settings: new SettingService(erpProvider),
+    addresses: new AddressService(erpProvider),
+    sales: new SaleService(erpProvider),
+    deliveryCarriers: new DeliveryCarrierService(erpProvider),
+    payment: new PaymentService(erpProvider),
+    customer: new CustomerService(erpProvider),
+    leads: new LeadsService(erpProvider)
   }
 
   // The following hook is used to let children add custom services to the app
@@ -112,21 +127,11 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   }
 })
 
-declare global {
-  interface Shopinvader {
-    services: ShopinvaderServiceList
-    providers: ShopinvaderProvidersList
-  }
-}
-
-declare module '#app' {
-  interface NuxtApp {
-    $shopinvader: Shopinvader
-  }
-}
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $shopinvader: Shopinvader
+const isValidURL = (url: string) => {
+  try {
+    new URL(url)
+    return true
+  } catch (e) {
+    return false
   }
 }
