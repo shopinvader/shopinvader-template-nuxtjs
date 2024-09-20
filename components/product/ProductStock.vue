8dd8488b82c0a6ba3810aaabd9b242ecb0476516
egervy <gervy.emmanuel@gmail.com>
2024-09-20 14:57:27 +0200

    fix: limited stock display used a wrongly named state and the icon was badly displayed

--- components/product/ProductStock.vue
+++ components/product/ProductStock.vue
@@ -3,51 +3,48 @@
     <div class="stock-status">
       <div v-if="state == 'in_stock'" class="stock-status__available">
         <icon name="check" class="stock-icon" />
-        <span class="stock-text">{{ $t('product.stock.available') }}</span>
+        <span class="stock-text">{{ t('product.stock.available') }}</span>
       </div>
-      <div v-else-if="state == 'in_limited'" class="stock-status__ending">
+      <div v-else-if="state == 'in_limited_stock'" class="stock-status__ending">
         <icon name="attention" class="stock-icon" />
-        <span class="stock-text">{{ $t('product.stock.ending_soon') }}</span>
+        <span class="stock-text">{{ t('product.stock.ending_soon') }}</span>
       </div>
       <div v-else-if="state == 'resupplying'" class="stock-status__ending">
         <icon name="attention" class="stock-icon" />
-        <span class="stock-text">{{ $t('product.stock.resupplying') }}</span>
+        <span class="stock-text">{{ t('product.stock.resupplying') }}</span>
       </div>
       <div v-else-if="state == 'out_of_stock'" class="stock-status__out-of-stock">
         <icon name="entypo:cycle" class="stock-icon" />
-        <span class="stock-text">{{ $t('product.stock.not_available') }}</span>
+        <span class="stock-text">{{ t('product.stock.not_available') }}</span>
       </div>
     </div>
   </slot>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import type { ProductStock } from '#models'
 import type { PropType } from 'vue'
+const { t } = useI18n()
 
-export default {
-  name: 'ProductStock',
-  props: {
-    stock: {
-      type: Object as PropType<ProductStock>,
-      required: false,
-      default: () => {
-        return null
-      }
-    }
-  },
-  computed: {
-    state(): string | null {
-      let state = this.stock?.global?.state || null
-      if (!state && this.qty) {
-        state = this.qty > 0 ? 'in_stock' : 'out_of_stock'
-      }
-      return state
-    },
-    qty(): number | null {
-      return this.stock?.global?.qty || null
+const props = defineProps({
+  stock: {
+    type: Object as PropType<ProductStock>,
+    required: false,
+    default: () => {
+      return null
     }
   }
-}
+})
+
+const state = computed<string | null>(() => {
+  let state = props.stock?.global?.state || null
+  if (!state && qty) {
+    state = qty.value !== null && qty.value > 0 ? 'in_stock' : 'out_of_stock'
+  }
+  return state
+})
+const qty = computed<number | null>(() => {
+  return props.stock?.global?.qty || null
+})
 </script>
 <style lang="scss">
 .stock-status {
@@ -61,7 +58,7 @@ export default {
     }
   }
   &__ending {
-    @apply max-w-max py-1  text-sm text-primary;
+    @apply flex max-w-max items-center py-1 text-sm text-primary;
 
     .stock-icon {
       @apply mr-1 inline text-xl text-error;
@@ -71,16 +68,16 @@ export default {
     }
   }
   &__resupplying {
-    @apply max-w-max py-1  text-sm text-info;
+    @apply max-w-max py-1 text-sm text-info;
     .stock-icon {
-      @apply mr-1 inline text-xl text-error;
+      @apply mr-1 inline-block text-xl text-error;
     }
     .stock-text {
       @apply text-error;
     }
   }
   &__out-of-stock {
-    @apply max-w-max py-1  text-sm text-primary;
+    @apply max-w-max py-1 text-sm text-primary;
     .stock-icon {
       @apply mr-1 inline text-xl text-error;
     }
