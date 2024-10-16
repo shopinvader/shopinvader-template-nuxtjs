7bb469999f74f56b5349f35e0e3da0c6be1444c9
Thibault Rey <thibault.rey@akretion.com>
2024-10-16 19:02:25 +0200

    fix(password): fix blank content on reset password page

--- pages/account/password-reset.vue
+++ pages/account/password-reset.vue
@@ -1,20 +1,28 @@
 <template>
   <nuxt-layout name="login">
     <template #body>
-      <div class="card card-body card-bordered bg-white max-w-2xl">
-        <account-password-reset ></account-password-reset>
+      <div class="card card-body card-bordered max-w-2xl bg-white">
+        <account-password-reset></account-password-reset>
       </div>
     </template>
   </nuxt-layout>
 </template>
 <script setup lang="ts">
+// check if a user is currently loggued in and redirect to account page
 const auth = useShopinvaderService('auth')
+const localePath = useLocalePath()
 if (auth?.type !== 'credentials') {
   /** back to home if is not auth provider credential */
-  await navigateTo({ path: '/' })
+  await navigateTo(localePath('/'))
 }
-
+const { t } = useI18n()
 definePageMeta({
-  layout: false
+  layout: 'empty'
+})
+useSeoMeta({
+  title: t('account.reset.reset_pswd'),
+  ogTitle: t('account.reset.reset_pswd'),
+  description: t('account.reset.description'),
+  ogDescription: t('account.reset.description')
 })
 </script>
