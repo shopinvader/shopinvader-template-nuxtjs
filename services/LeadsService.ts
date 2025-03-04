33c565b42745eab5883d6932959530de163a36cf
Thibault Rey <thibault.rey@akretion.com>
2025-03-04 14:42:08 +0100

    fix (lead) : fix lead service

--- services/LeadsService.ts
+++ services/LeadsService.ts
@@ -1,31 +1,18 @@
 import { Lead, type LeadsResult } from '#models'
-import { BaseServiceLocalized } from '#services'
-import type { $Fetch } from 'ofetch'
+import { BaseServiceErp } from '#services'
 
-
-export class LeadsService extends BaseServiceLocalized {
-  public ofetch: $Fetch
-  // Base url of the erp API (eg; https://myerp.mycompany.com/shopinvader)
-  public erpBaseUrl: string
-  // Note: you can change the endpoint to match your API endpoint before the Init
-  public endpoint: string = ''
-  // The urlEndpoint is the erpBaseUrl + the endpoint
-  public urlEndpoint: string = ''
-  constructor(isoLocale: string, ofetch: $Fetch, erpBaseUrl: string) {
-    super(isoLocale)
-    this.ofetch = ofetch
-    this.erpBaseUrl = erpBaseUrl
-  }
+export class LeadsService extends BaseServiceErp {
+  public override endpoint: string = 'leads'
 
   async create(lead: Lead, files: FileList | null): Promise<LeadsResult> {
     const data = lead.getJSONData()
     const formData = new FormData()
-    formData.append("data", JSON.stringify(data))
+    formData.append('data', JSON.stringify(data))
     if (files) {
       formData.append(`files`, files[0])
     }
-    const result = await this.ofetch("leads", {
-      method: "POST",
+    const result = await this.ofetch('leads', {
+      method: 'POST',
       body: formData
     })
     return {
