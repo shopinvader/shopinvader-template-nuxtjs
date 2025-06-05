5fa3a6d28bdbffcdceff1071dc8d9b4cd46b687e
Dora Jurcevic <dora.jurcevic@akretion.com>
2025-06-05 14:33:21 +0200

    FIX: cartline qty readonly false forced removed

--- components/global/InputQty.vue
+++ components/global/InputQty.vue
@@ -114,7 +114,7 @@ export default {
 </script>
 <style lang="scss">
 .input-qty {
-  @apply input input-bordered flex items-center gap-2;
+  @apply  input input-bordered  flex items-center ;
   &__input {
     @apply grow text-center;
     appearance: textfield;
