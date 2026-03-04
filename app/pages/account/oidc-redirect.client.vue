1eb04b8b973c3397f7c96d8d1e8eb96164ca57aa
Thibault Rey <thibault.rey@akretion.com>
2026-03-04 12:26:15 +0100

    fix(auth): prevent error if redirection URL after OIDC login is external

--- app/pages/account/oidc-redirect.client.vue
+++ app/pages/account/oidc-redirect.client.vue
@@ -11,11 +11,20 @@ definePageMeta({
 // to the right page while the OIDC provider manage the token (before this).
 
 onMounted(async () => {
+  let external = false
   // Get the target page from the querystring
   const target = new URLSearchParams(window.location.search).get('target')
-  const decodedTarget = target ? decodeURIComponent(target) : '/'
+  let decodedTarget = target ? decodeURIComponent(target) : '/'
+  // check if the target is on the same domain
+  if (decodedTarget.startsWith('http')) {
+    external = !decodedTarget.startsWith(window.location.origin)
+    if (!external) {
+      decodedTarget = decodedTarget.replace(window.location.origin, '')
+    }
+  }
+
   // Redirect to the target page
-  await navigateTo(decodedTarget, { replace: true })
+  await navigateTo(decodedTarget, { external })
 })
 </script>
 <style lang="scss">
